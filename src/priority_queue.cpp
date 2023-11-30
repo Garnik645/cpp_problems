763861e332ae74fcc2f4d6356814e37a8b9e75ea
Garnik Khroyan <44695597+Garnik645@users.noreply.github.com>
2023-11-30 23:01:37 +0400

    Create priority_queue.cpp

---
+++ src/priority_queue.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+
+class SeatManager {
+  std::priority_queue<int, std::vector<int>, std::greater<>> pq;
+
+public:
+  SeatManager(int n) {
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    for (int i = 1; i <= n; ++i) {
+      pq.push(i);
+    }
+  }
+
+  int reserve() {
+    int tmp = pq.top();
+    pq.pop();
+    return tmp;
+  }
+
+  void unreserve(int seatNumber) {
+    pq.push(seatNumber);
+  }
+};
